64c676a861185104ead0ff60a1b9141fb5a59e2d
Rodrigo Luis Ribeiro <rodrigo.ribeiro0909@gmail.com>
2022-05-14 16:04:08 -0300

    Testing components

---
+++ control-manager-services/src/control-manager-services/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './components/my-first-component/my-first-component.component';
+import { MySecondComponentComponent } from './components/my-second-component/my-second-component.component';
+import { MyThirdComponentComponent } from './components/my-third-component/my-third-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyFirstComponentComponent,
+    MySecondComponentComponent,
+    MyThirdComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

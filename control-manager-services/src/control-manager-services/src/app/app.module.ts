033563f0978ca7f73d06ff70ae233012b5b253c6
Rodrigo Ribeiro <rodrigo.ribeiro0909@gmail.com>
2022-05-20 22:13:07 -0300

    feature-atividade-06

--- control-manager-services/src/control-manager-services/src/app/app.module.ts
+++ control-manager-services/src/control-manager-services/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { MyFirstComponentComponent } from './components/my-first-component/my-first-component.component';
 import { MySecondComponentComponent } from './components/my-second-component/my-second-component.component';
 import { MyThirdComponentComponent } from './components/my-third-component/my-third-component.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFirstComponentComponent,
     MySecondComponentComponent,
-    MyThirdComponentComponent
+    MyThirdComponentComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
